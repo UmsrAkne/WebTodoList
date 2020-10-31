deb14ca5822e3781c9a546dd215280bcf5d7c30a
Akn <sibata486@gmail.com>
2020-10-31 14:34:08 +0900

    クラス追加 / Todo とは別扱いのコメントを表すクラスを追加

---
+++ WebTodoApp/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebTodoApp.Models {
+    public class Comment {
+        public int ID { get; set; }
+        public DateTime CreationDateTime { get; set; }
+        public String TextContent { get; set; }
+    }
+}

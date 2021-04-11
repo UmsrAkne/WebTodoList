3af6eef4e9c11a6041593475dd6c734f217ae258
Akn <sibata486@gmail.com>
2021-04-11 19:57:32 +0900

    暗号鍵を外部からセットできるようプロパティを追加

--- WebTodoApp/Models/Encryptor.cs
+++ WebTodoApp/Models/Encryptor.cs
@@ -10,13 +10,16 @@ namespace WebTodoApp.Models {
     public class Encryptor {
 
         public string encrypt(string plainText) {
+            if(Key.Length == 0) {
+                Key = getKey();
+            }
+
             byte[] src = Encoding.Unicode.GetBytes(plainText);
 
-            byte[] key = getKey();
             byte[] iv = new byte[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 
             using (var aesManaged = new AesManaged())
-            using (var encryptor = aesManaged.CreateEncryptor(key, iv))
+            using (var encryptor = aesManaged.CreateEncryptor(Key, iv))
             using (var outStream = new MemoryStream()) {
                 using (var cryptoStream = new CryptoStream(outStream, encryptor, CryptoStreamMode.Write)) {
                     cryptoStream.Write(src, 0, src.Length);
@@ -28,14 +31,16 @@ namespace WebTodoApp.Models {
         }
 
         public string decrypt(string encryptText) {
-            byte[] key = getKey();
-            byte[] iv = new byte[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            if(Key.Length == 0) {
+                Key = getKey();
+            }
 
+            byte[] iv = new byte[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 
             byte[] src = Convert.FromBase64String(encryptText);
 
             using (var aesManaged = new AesManaged())
-            using (var decryptor = aesManaged.CreateDecryptor(key, iv))
+            using (var decryptor = aesManaged.CreateDecryptor(Key, iv))
             using (var inStream = new MemoryStream(src,false))
             using (var outStream = new MemoryStream()) {
                 using (var cryptoStream = new CryptoStream(inStream, decryptor, CryptoStreamMode.Read)) {
@@ -51,8 +56,14 @@ namespace WebTodoApp.Models {
                 byte[] result = outStream.ToArray();
                 return Encoding.Unicode.GetString(result);
             }
-
         }
+
+        /// <summary>
+        /// 暗号、復号時に使用するキーをセットします。バイト配列のサイズは 32 でセットします。
+        /// このプロパティにはデフォルトで要素数０のバイト配列が入っています。
+        /// デフォルトの配列がセットされている場合は、コンピューター名から生成されるデフォルトの暗号鍵を使用して暗号化、復号を行います。
+        /// </summary>
+        public byte[] Key { get; set; } = new byte[0];
 
         private string getHash(string text) {
             var sha256 = SHA256.Create();

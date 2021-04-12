eccdc3ec27965203a7e52d62586426105368705d
Akn <sibata486@gmail.com>
2021-04-12 23:44:05 +0900

    クラス追加 / 任意のDBサーバーへの接続情報を保持するクラスを追加

---
+++ WebTodoApp/Models/AnyDBConnectionStrings.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebTodoApp.Models {
+    class AnyDBConnectionStrings : BindableBase, IDBConnectionStrings{
+        public string UserName { get => userName; set => SetProperty(ref userName, value); }
+        private string userName = "";
+
+        public string PassWord { get => password; set => SetProperty(ref password, value); }
+        private string password = "";
+
+        public string HostName { get => hostName; set => SetProperty(ref hostName, value); }
+        private string hostName = "";
+
+        public int PortNumber { get => portNumber; set => SetProperty(ref portNumber, value); }
+        private int portNumber = 5432;
+
+        public string ServiceName { get; set; }
+    }
+}
--- WebTodoApp/ViewModels/ConnectionDialogViewModel.cs
+++ WebTodoApp/ViewModels/ConnectionDialogViewModel.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebTodoApp.Models;
 
 namespace WebTodoApp.ViewModels {
     class ConnectionDialogViewModel : IDialogAware {
         public string Title => "サーバーの情報を入力";
 
         public event Action<IDialogResult> RequestClose;
+
+        public AnyDBConnectionStrings DBConnectionStrings = new AnyDBConnectionStrings();
 
         public bool CanCloseDialog() => true;
 
